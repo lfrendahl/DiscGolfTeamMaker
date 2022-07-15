d4d6b1c556280cc4cec0ef38f7418debb92ab365
lfrendahl <lfrendahl3@gmail.com>
2022-07-14 21:56:40 -0400

    Added advanced style and media query

--- js/main.js
+++ js/main.js
@@ -1,10 +1,39 @@
-const discs = ["Buzz", "Paradox", "River"];
+const discs = ["Entropy", "Cosmic Neutron Envy", "Soft Neutron Envy", "Proxy", "Pyro", "Reactor", "Hex", "Uplink", "Paradox", "Resistor", "Crave", "Relay", "Signal", "Volt", "Inertia", "Insanity", "SE Virus", "Virus", "Photon", "Wave", "Orbital", "Relativity"];
 
-document.querySelector('#mysterybutton').addEventListener('click', discRandomizer);
 
-function discRandomizer () {
-    let choice =  (Math.floor(Math.random() * discs.length))
-    console.log(choice)
-    console.log(discs[choice])
-    document.querySelector('h3').innerText = discs[choice]
+document.querySelector('#mysterybutton').addEventListener('click', interval);
+
+
+function interval() {
+    let count = 0;
+    let flashing = setInterval(function (){
+    count += 1;
+       if(count === 8){
+        clearInterval(flashing);
+    }
+  
+    let choice = (Math.floor(Math.random() * discs.length));
+    document.querySelector('h3').innerText = discs[choice];
+
+}, 25);
 }
+
+//function discRandomizer () {
+//      let choice = (Math.floor(Math.random() * discs.length))
+//    document.querySelector('h3').innerText = discs[choice]
+//}
+
+//function loading () {
+//    for(var i = 0;i < 4;i++) {
+//        let choice = (Math.floor(Math.random() * discs.length));
+//        document.querySelector('h3').innerText = discs[choice];
+//    }
+//}
+
+
+
+
+ //function changeStyle(){
+ //       var element = document.getElementById("myElement");
+ //       element.style.color = "#FF0000";
+ //   }
